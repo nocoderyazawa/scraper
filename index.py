2a65b0add33affe98aaaaf5185b413bfa746f793
nocoderyazawa <nocoderyazawa@gmail.com>
2020-05-04 12:39:11 +0900

    add scraping scripts to index.py

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
 print("hello git!")
+
+import requests
+from bs4 import BeautifulSoup
+ 
+r = requests.get("https://news.yahoo.co.jp/")
+ 
+soup = BeautifulSoup(r.content, "html.parser")
+ 
+# ニュース一覧を抽出
+print(soup.find("ul", "newsFeed_list"))
